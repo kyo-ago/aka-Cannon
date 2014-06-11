cefdaceafab423c1c807a8d18119fd212307881c
kyo-ago <kyo@0-9.sakura.ne.jp>
2014-06-11 20:48:53 +0900

    WIP

--- web_accessible_resources/index.js
+++ web_accessible_resources/index.js
@@ -1,10 +1,24 @@
 (function () {
     var notificationChecked = JSON.parse(localStorage['akaCannon'] || '{}');
+    (function () {
+        Object.keys(notificationChecked).forEach(function (key) {
+            var val = notificationChecked[key];
+            if ('boolean' === typeof val) {
+                notificationChecked[key] = {
+                    'disabled' : val,
+                    'notifyText' : []
+                }
+            }
+        });
+    })();
 
     overwriteNotification();
 
     window.addEventListener('hashchange', function () {
-        setTimeout(initCheckbox, 100);
+        setTimeout(function () {
+            initCheckbox();
+            initTextarea();
+        }, 100);
     });
 
     var interval = setInterval(function () {
@@ -24,17 +38,30 @@
 
         var span = document.createElement('span');
         var input = document.createElement('input');
+        var textarea = document.createElement('textarea');
         var text = document.createTextNode('Notify');
         input.setAttribute('type', 'checkbox');
         input.setAttribute('id', 'enableNotification');
+        textarea.setAttribute('id', 'notificationText');
+        textarea.setAttribute('placeholder', 'Notification texts("\\n" separete)');
+        textarea.setAttribute('style', 'vertical-align: top;');
+        textarea.addEventListener('focus', function () {
+            this.style.height = '400px';
+        });
+        textarea.addEventListener('blur', function () {
+            this.style.height = '';
+        });
         span.setAttribute('style', 'margin: 0 10px; font-size: 0.9em;');
         span.appendChild(text);
         span.appendChild(input);
+        span.appendChild(textarea);
         var HeaderBtn = document.querySelector('.chatRoomHeaderBtn');
         HeaderBtn.insertBefore(span, HeaderBtn.firstElementChild);
 
         initCheckbox();
+        initTextarea();
         input.addEventListener('change', changeCheckbox);
+        textarea.addEventListener('change', changeTextarea);
         return true;
     }
 
@@ -44,29 +71,70 @@
         if (!input || !_roomTitleText) {
             return;
         }
-        input.checked = !notificationChecked[_roomTitleText.textContent];
+        var text = _roomTitleText.textContent;
+        var notificationText = document.getElementById('notificationText');
+
+        notificationChecked[text] = notificationChecked[text] || {};
+        input.checked = !notificationChecked[text]['disabled'];
+        notificationText.disabled = notificationChecked[text]['disabled'];
+    }
+
+    function initTextarea () {
+        var _roomTitleText = document.querySelector('#_roomTitle ._roomTitleText');
+        if (!_roomTitleText) {
+            return;
+        }
+        var text = _roomTitleText.textContent;
+        var notificationText = document.getElementById('notificationText');
+
+        notificationChecked[text] = notificationChecked[text] || {};
+        notificationText.value = notificationChecked[text]['notifyText'] || '';
     }
 
     function changeCheckbox (e) {
         e.preventDefault();
         e.stopPropagation();
         var _roomTitleText = document.querySelector('#_roomTitle ._roomTitleText');
-        notificationChecked[_roomTitleText.textContent] = !this.checked;
-        if (!notificationChecked[_roomTitleText.textContent]) {
-            delete notificationChecked[_roomTitleText.textContent];
-        }
+        var text = _roomTitleText.textContent;
+        var notificationText = document.getElementById('notificationText');
+
+        notificationChecked[text] = notificationChecked[text] || {};
+        notificationChecked[text]['disabled'] = !this.checked;
+        notificationText.disabled = !this.checked;
+        localStorage['akaCannon'] = JSON.stringify(notificationChecked);
+    }
+
+    function changeTextarea (e) {
+        e.preventDefault();
+        e.stopPropagation();
+        var _roomTitleText = document.querySelector('#_roomTitle ._roomTitleText');
+        var text = _roomTitleText.textContent;
+        notificationChecked[text] = notificationChecked[text] || {};
+        notificationChecked[text]['notifyText'] = this.value;
         localStorage['akaCannon'] = JSON.stringify(notificationChecked);
     }
 
     function overwriteNotification () {
-        var createNotification = window.webkitNotifications.createNotification;
-        window.webkitNotifications.createNotification = function (icon, title, message) {
-            if (notificationChecked[title]) {
-                return {
-                    'show' : function () {}
-                };
+        var createNotification = window.NotificationAPI.createNotification;
+        var emptyResponse = {
+            'show' : function () {}
+        };
+        window.NotificationAPI.createNotification = function (icon, title, message) {
+            if (!notificationChecked[title]) {
+                return reateNotification.apply(window.NotificationAPI, arguments);
             }
-            return createNotification.apply(window.webkitNotifications, arguments);
+            if (notificationChecked[title]['disabled']) {
+                return emptyResponse;
+            }
+            if (!notificationChecked[title]['notifyText']) {
+                return createNotification.apply(window.NotificationAPI, arguments);
+            }
+            if (notificationChecked[title]['notifyText'].some(function (ignore) {
+                return ~title.indexOf(ignore);
+            })) {
+                return emptyResponse;
+            }
+            return createNotification.apply(window.NotificationAPI, arguments);
         };
     }
 })();
